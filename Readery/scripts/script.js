9528798f6ce0a8ea4475d40d6568eece08857a5c
EDUARDO BORGES LORDEIRO DIAS <eduborgesbldias@gmail.com>
2025-10-03 10:50:25 -0300

    cards quase prontos

--- Readery/scripts/script.js
+++ Readery/scripts/script.js
@@ -1,3 +1,5 @@
+//* variaveis de botoes do card
+
 let button = document.getElementById('botao-card');
 
 let estrela1 = document.getElementById('1');
@@ -14,27 +16,149 @@ let estrela55 = document.querySelector(".estrela5");
 
 let nota = document.querySelector(".nota");
 
-button.addEventListener('click', function(){
-    let card = document.querySelector(".card-dentro");
-    card.classList.toggle('active');
-    if(button.textContent == "↑"){
-        button.textContent = "↓";
-    }
-    else{
-        button.textContent = "↑";
-    }
+
+const elBotaoAdiciona = document.getElementById('adiciona-livro');
+const elFiltroGenero = document.getElementById('filtro-genero');
+const elSecaoLivros = document.querySelector(".secao-livros");
+const imagens = document.querySelector(".img-card2");
+
+let listaLivros = [];
+
+elBotaoAdiciona.addEventListener('click', function () {
+    const nova = { id: Date.now(), titulo: "A Divina Comédia", status: "lendo", autor: "Dante Alighieri", genero: "poesia" }
+    listaLivros.push(nova);
+
+    console.log(listaLivros);
+    render();
 });
+
+function render() {
+
+    const filtro = elFiltroGenero.value;
+
+    const filtradas = listaLivros.filter(function (t) {
+        const okGenero = filtro === "todos" ? true : t.genero === filtro;
+
+        return okGenero;
+    });
+
+    elSecaoLivros.innerHTML = "";
+
+    filtradas.forEach(function (t) {
+
+
+        const div = document.createElement("div");
+        div.className = "espaco-card";
+
+        const divCard = document.createElement("div");
+        divCard.className = "card-dentro";
+
+        const divLixeira = document.createElement("div");
+        divLixeira.className = "lixeira";
+
+        const botaoRemover = document.createElement("button");
+        botaoRemover.textContent = "X";
+        botaoRemover.className = "remover";
+
+        botaoRemover.addEventListener('click', function () {
+            modal.showModal();
+        })
+
+        const modal = document.createElement("dialog");
+        modal.className = 'meu-modal';
+
+        const textoModal = document.createElement("h1");
+        textoModal.className = "texto-modal";
+        textoModal.textContent = "Deseja mesmo excluir o livro?";
+
+        const botaoCancelarModal = document.createElement("button");
+        botaoCancelarModal.className = "botao-cancela-modal";
+        botaoCancelarModal.textContent = "Cancelar";
+
+        botaoCancelarModal.addEventListener('click', function () {
+            modal.close();
+        })
+
+        const botaoExcluirModal = document.createElement("button");
+        botaoExcluirModal.className = "botao-exclui-modal";
+        botaoExcluirModal.textContent = "Excluir";
+
+        botaoExcluirModal.addEventListener('click', function () {
+            listaLivros = listaLivros.filter(apagar => apagar.id !== t.id);
+            modal.close();
+            render();
+        })
+
+        const divModal = document.createElement("div");
+        divModal.className = "div-Modal";
+
+        const botaoImagemCard = document.createElement("button");
+        botaoImagemCard.className = "botao-imagem-card";
+
+        const imagem = document.createElement("img");
+        imagem.src = "assets/img/placeholder.svg";
+        imagem.className = "img-card";
+
+        const divTextos = document.createElement("div");
+        divTextos.className = "textos";
+
+        const textoCard = document.createElement("p");
+        textoCard.className = "text-card";
+        textoCard.textContent = "Lorem Ipsum is simply dummy text of the printing and ty Lorem Ipsum Dolor is simply.Lorem Ipsum is simply dummy text of the printing and ty Lorem Ipsum Dolor is simply.Lorem Ipsum is simply dummy text of the printing and ty Lorem Ipsum Dolor is simply."
+
+        const botaoCard = document.createElement("button");
+        botaoCard.className = "botao-card";
+        botaoCard.textContent = "↓";
+
+        botaoCard.addEventListener('click', function () {
+            let card = document.querySelector(".card-dentro");
+            card.classList.toggle('active');
+            if (botaoCard.textContent == "↑") {
+                botaoCard.textContent = "↓";
+            }
+            else {
+                botaoCard.textContent = "↑";
+            }
+        });
+
+
+        divCard.appendChild(divLixeira);
+        divCard.appendChild(botaoImagemCard);
+        divCard.appendChild(divTextos);
+        divCard.appendChild(botaoCard);
+        divCard.appendChild(modal);
+
+        botaoImagemCard.appendChild(imagem);
+
+        modal.appendChild(textoModal);
+        modal.appendChild(divModal);
+        divModal.appendChild(botaoCancelarModal);
+        divModal.appendChild(botaoExcluirModal);
+
+
+        divLixeira.appendChild(botaoRemover);
+        divTextos.appendChild(textoCard);
+
+        div.appendChild(divCard);
+        elSecaoLivros.appendChild(div);
+
+    });
+}
+
+
+
+
+
 
 //* estrelas
 
-estrela1.addEventListener('click', function(){
-    
+estrela1.addEventListener('click', function () {
 
-    if(estrela.textContent == "☆"){
+    if (estrela.textContent == "☆") {
         estrela.textContent = "★";
         nota.textContent = "1/5";
     }
-    else if(estrela.textContent == "★"){
+    else if (estrela.textContent == "★") {
         estrela.textContent = "☆";
         estrela22.textContent = "☆";
         estrela33.textContent = "☆";
@@ -44,14 +168,14 @@ estrela1.addEventListener('click', function(){
     }
 })
 
-estrela2.addEventListener('click', function(){
-    
-    if(estrela22.textContent == "☆"){
+estrela2.addEventListener('click', function () {
+
+    if (estrela22.textContent == "☆") {
         estrela.textContent = "★"
         estrela22.textContent = "★";
         nota.textContent = "2/5";
     }
-    else if(estrela22.textContent == "★"){
+    else if (estrela22.textContent == "★") {
         estrela22.textContent = "☆";
         estrela33.textContent = "☆";
         estrela44.textContent = "☆";
@@ -60,15 +184,15 @@ estrela2.addEventListener('click', function(){
     }
 })
 
-estrela3.addEventListener('click', function(){
-    
-    if(estrela33.textContent == "☆"){
+estrela3.addEventListener('click', function () {
+
+    if (estrela33.textContent == "☆") {
         estrela.textContent = "★"
         estrela22.textContent = "★";
         estrela33.textContent = "★";
         nota.textContent = "3/5";
-    }   
-    else if(estrela33.textContent == "★"){
+    }
+    else if (estrela33.textContent == "★") {
         estrela33.textContent = "☆"
         estrela44.textContent = "☆";
         estrela55.textContent = "☆";
@@ -76,25 +200,25 @@ estrela3.addEventListener('click', function(){
     }
 })
 
-estrela4.addEventListener('click', function(){
-    
-    if(estrela44.textContent == "☆"){
+estrela4.addEventListener('click', function () {
+
+    if (estrela44.textContent == "☆") {
         estrela.textContent = "★"
         estrela22.textContent = "★";
         estrela33.textContent = "★";
         estrela44.textContent = "★";
         nota.textContent = "4/5";
     }
-    else if(estrela44.textContent == "★"){
+    else if (estrela44.textContent == "★") {
         estrela44.textContent = "☆";
         estrela55.textContent = "☆";
         nota.textContent = "3/5";
     }
 })
 
-estrela5.addEventListener('click', function(){
-    
-    if(estrela55.textContent == "☆"){
+estrela5.addEventListener('click', function () {
+
+    if (estrela55.textContent == "☆") {
         estrela.textContent = "★"
         estrela22.textContent = "★";
         estrela33.textContent = "★";
@@ -102,7 +226,7 @@ estrela5.addEventListener('click', function(){
         estrela55.textContent = "★";
         nota.textContent = "5/5";
     }
-    else if(estrela55.textContent == "★"){
+    else if (estrela55.textContent == "★") {
         estrela55.textContent = "☆";
         nota.textContent = "4/5";
     }
