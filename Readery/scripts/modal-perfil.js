c0820437f99b711857dc39bf10f221bb233aa1f6
Isaque-Codes <isaque.137.silva@gmail.com>
2025-12-05 10:23:35 -0300

    feat: Construcao inicial do modal

---
+++ Readery/scripts/modal-perfil.js
@@ -0,0 +1,13 @@
+const modal = document.getElementById('modal-perfil');
+const botaoAbrir = document.getElementById('abrir-modal');
+const botaoFechar = document.getElementById('fechar-modal');
+
+// Abrir o modal quando o botão for clicado
+botaoAbrir.addEventListener('click', () => {
+    modal.showModal(); // Use showModal() para o comportamento de modal
+});
+
+// Fechar o modal quando o botão de fechar for clicado
+botaoFechar.addEventListener('click', () => {
+    modal.close();
+});
