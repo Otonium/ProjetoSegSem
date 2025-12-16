683ccc467c9ef65a1dfef800e94f81b3259bc44e
unknown <ola.antoniocarlos@gmail.com>
2025-12-16 16:23:27 -0300

    Reconstrução da branch para reduzir comentários não profissionais

---
+++ HomepageReadery-main -MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HomepageReadery_main__MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
